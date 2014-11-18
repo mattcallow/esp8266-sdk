68ef8c81aa3c9258e201754654327dcbd47ba952
Matt Callow <mattcallow@github.com>
2014-11-18 11:58:41 +1100

    Added example config

--- 06thingspeak/include/user_config.h
+++ 06thingspeak/include/user_config.h
@@ -1,1 +1,8 @@
-// blank
+#ifndef _USER_CONFIG_H_
+#define _USER_CONFIG_H_
+
+#error Change WIFI_SSID and WIFI_PASSWORD to suit your network (and delete this line)
+#define WIFI_SSID "changeme"
+#define WIFI_PASSWORD "changeme"
+
+#endif
